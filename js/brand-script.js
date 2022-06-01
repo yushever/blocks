95da355ce18f58b7784f4d1fa6d816ebf92bd7b8
yushever <iushina.tatiana@gmail.comgit config --global user.email iushina.tatiana@gmail.comgit config --global user.email iushina.tatiana@gmail.com>
2022-06-01 19:33:36 +0300

    comments removed

--- js/brand-script.js
+++ js/brand-script.js
@@ -1,20 +1,8 @@
-// new Swiper('.swiper', {
-//     pagination: {
-//         el: '.swiper-pagination',
-//     },
-// });
+let init = false;
 
-/* Swiper
-**************************************************************/
-var swiper = Swiper;
-var init = false;
-
-/* Which media query
-**************************************************************/
 function swiperMode() {
     let mobile = window.matchMedia('(min-width: 0px) and (max-width: 768px)');
-    let tablet = window.matchMedia('(min-width: 769px) and (max-width: 1024px)');
-    let desktop = window.matchMedia('(min-width: 1025px)');
+    let desktop = window.matchMedia('(min-width: 769px)');
 
     // Enable (for mobile)
     if(mobile.matches) {
@@ -22,52 +10,16 @@ function swiperMode() {
             init = true;
             swiper = new Swiper('.swiper', {
                 slidesPerView: 3,
-                centeredSlides: true,
+                centeredSlides: false,
+                initialSlide: 0,
                 loop: true,
                 spaceBetween: 10,
-                direction: 'horizontal',
-                effect: 'coverflow',
                 pagination: {
                     clickable: true,
                     el: '.swiper-pagination',
                   },   
-
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-
-                coverflowEffect: {
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 0,
-                    modifier: 0,
-                    slideShadows: false,
-                },
-
-                breakpoints: {
-
-                    767: {
-                        slidesPerView: 1,
-                        spaceBetween: 0,
-                        effect: 'coverflow',
-
-                        coverflowEffect: {
-                            rotate: 0,
-                            stretch: 20,
-                            depth: 120,
-                            modifier: 3,
-                            slideShadows: false,
-                        }
-                    }
-                }
             });
         }
-    }
-    // Disable (for tablet)
-    else if(tablet.matches) {
-        swiper.destroy();
-        init = false;
     }
     // Disable (for desktop)
     else if(desktop.matches) {
